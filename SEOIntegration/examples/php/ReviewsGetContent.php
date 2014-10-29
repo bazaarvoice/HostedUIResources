06859eda6ffb243db28d10754702cb91d9eda91e
Jared Enriquez <jared.enriquez@bazaarvoice.com>
2014-10-29 16:47:18 -0500

    Create ReviewsGetContent.php

---
+++ SEOIntegration/examples/php/ReviewsGetContent.php
@@ -0,0 +1,24 @@
+<?php
+//Please provide cloud_key, deployment_zone_id and product_id
+require('bvseosdk.php');
+    $bv = new BV(array(
+     'deployment_zone_id' => '',
+     'product_id' => '',
+     'cloud_key' => '',
+     'current_page_url' => '',
+    ));
+?>
+
+<html>
+<head>
+    <title>BV SDK PHP Example - GetContent</title>
+</head>
+<body>
+    This is a test page for Reviews: getContent() <br>
+    GetContent() will return reviews and aggregate content <br>
+
+    <div id="BVRRContainer">
+      <?php echo $bv->reviews->getContent();?>
+    </div>
+</body>
+</html>
