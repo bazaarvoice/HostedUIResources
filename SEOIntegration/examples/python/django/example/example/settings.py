d403b97eb62cdc4172b788f8c7805a6f25d33030
Jeremy A. Love <jeremy.love@bazaarvoice.com>
2016-09-12 15:24:49 -0500

    replaced sample domains with example.com

--- SEOIntegration/examples/python/django/example/example/settings.py
+++ SEOIntegration/examples/python/django/example/example/settings.py
@@ -44,22 +44,22 @@ USE_L10N = True
 USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
+# Example: "/home/media/media.example.com/media/"
 MEDIA_ROOT = ''
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+# Examples: "http://media.example.com/media/", "http://example.com/media/"
 MEDIA_URL = ''
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
+# Example: "/home/media/media.example.com/static/"
 STATIC_ROOT = ''
 
 # URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
+# Example: "http://media.example.com/static/"
 STATIC_URL = '/static/'
 
 # Additional locations of static files
